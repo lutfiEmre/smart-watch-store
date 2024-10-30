37df36354ba9e1e46016bf4eaa67d4df48bc64cf
Emre Lutfi <emrelutfi@Emre-MacBook-Pro-2.local>
2024-10-30 14:25:26 +0300

    featureone

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import './css/stylized.css'
 import NavBar from "./components/NavBar.tsx";
 import Header from "./components/home/Header.tsx";
 import WatchesList from "./components/home/WatchesList.tsx";
+import FeatureOne from "./components/home/FeatureOne.tsx";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
           <div className={'w-full xl:px-[100px] lg:px-[100px]'}>
               <Header/>
               <WatchesList/>
+              <FeatureOne/>
           </div>
           <div className={'w-full h-[500px]'}></div>
       </>
---
+++ frontend/src/components/home/FeatureOne.tsx
@@ -0,0 +1,16 @@
+import featureimg from '../../assets/clocks/featureone.svg'
+
+const FeatureOne = () => {
+    return (
+        <div className={'mt-[112px] flex flex-row gap-[80px]'}>
+            <img src={featureimg} alt=""/>
+            <div className={'flex flex-col gap-[16px]'}>
+                <h6>
+                    asds
+                </h6>
+            </div>
+        </div>
+    );
+};
+
+export default FeatureOne;
